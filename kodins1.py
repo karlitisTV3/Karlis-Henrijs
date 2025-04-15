eef4bbe597d384824b0a12986d46e47c290e8b8e
henrijs <hegrevels@gmail.com>
2025-04-15 13:47:42 +0300

    dd

--- kodins1.py
+++ kodins1.py
@@ -42,3 +42,52 @@ print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣯⡿⣽⡾⣟⣿⢾⣽⣿�
 print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⣾⣿⣻⣽⣿⣯⣿⣿⣿⣿⣿⡍⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠁⠀⠀⠀⠀⠀")
 print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠉⠀⠀⠀⠀⠀⠀⠈⠉⠉⠁⠉⠈⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀")
 print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠉⠿⠛⠛⠛⠛⠛⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+
+import sqlite3
+
+# Izveido savienojumu ar datubāzi (fails tiks izveidots, ja neeksistē)
+conn = sqlite3.connect("pulcinu_pieteiksanas.db")
+cursor = conn.cursor()
+
+# Izveido tabulu, ja tā vēl neeksistē
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS pieteikumi (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+vards TEXT,
+uzvards TEXT,
+klase TEXT,
+novirziens TEXT,
+pulcins TEXT,
+ieprieks TEXT,
+informacijas_avots TEXT
+)
+''')
+# Funkcija pieteikumam
+
+def pieteikties_pulcinam():
+    print("== R6VSK Pulciņu Pieteikšanās ==")
+vards = input("Vārds: ")
+uzvards = input("Uzvārds: ")
+klase = input("Klase: ")
+novirziens = input("Pulciņa novirziens: ")
+pulcins = input("Izvēlētais pulciņš (piem. Datorzinības, Mūzika utt.): ")
+ieprieks = input("Vai esat piedalījies iepriekš? (jā/ne): ").lower()
+informacijas_avots = input("Kā uzzinājāt par pulciņu? (Mājaslapa, Draugi, Soc. tīkli, Cits): ")
+
+
+
+# Datu ievietošana datubāzē
+
+cursor.execute('''
+INSERT INTO pieteikumi (vards, uzvards, klase, novirziens, pulcins, ieprieks, informacijas_avots)
+VALUES (?, ?, ?, ?, ?, ?, ?)
+''', (vards, uzvards, klase, novirziens, pulcins, ieprieks, informacijas_avots))
+conn.commit()
+
+print("\nPaldies! Jūsu pieteikums tika saglabāts datubāzē.")
+# Izsauc funkciju
+pieteikties_pulcinam()
+
+# Aizver datubāzes savienojumu
+
+conn.close()
